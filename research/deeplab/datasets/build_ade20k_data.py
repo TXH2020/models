f694519a8610b3cf92a274f34ec9331716092037
TXH2020 <87963860+TXH2020@users.noreply.github.com>
2023-06-29 21:20:00 +0530

    Update build_ade20k_data.py

--- research/deeplab/datasets/build_ade20k_data.py
+++ research/deeplab/datasets/build_ade20k_data.py
@@ -27,28 +27,28 @@ import build_data
 from six.moves import range
 import tensorflow as tf
 
-FLAGS = tf.app.flags.FLAGS
+FLAGS = tf.compat.v1.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string(
+tf.compat.v1.app.flags.DEFINE_string(
     'train_image_folder',
     './ADE20K/ADEChallengeData2016/images/training',
     'Folder containing trainng images')
-tf.app.flags.DEFINE_string(
+tf.compat.v1.app.flags.DEFINE_string(
     'train_image_label_folder',
     './ADE20K/ADEChallengeData2016/annotations/training',
     'Folder containing annotations for trainng images')
 
-tf.app.flags.DEFINE_string(
+tf.compat.v1.app.flags.DEFINE_string(
     'val_image_folder',
     './ADE20K/ADEChallengeData2016/images/validation',
     'Folder containing validation images')
 
-tf.app.flags.DEFINE_string(
+tf.compat.v1.app.flags.DEFINE_string(
     'val_image_label_folder',
     './ADE20K/ADEChallengeData2016/annotations/validation',
     'Folder containing annotations for validation')
 
-tf.app.flags.DEFINE_string(
+tf.compat.v1.app.flags.DEFINE_string(
     'output_dir', './ADE20K/tfrecord',
     'Path to save converted tfrecord of Tensorflow example')
 
@@ -67,12 +67,12 @@ def _convert_dataset(dataset_split, dataset_dir, dataset_label_dir):
     RuntimeError: If loaded image and label have different shape.
   """
 
-  img_names = tf.gfile.Glob(os.path.join(dataset_dir, '*.jpg'))
+  img_names = tf.io.gfile.glob(os.path.join(dataset_dir, '*.jpg'))
   random.shuffle(img_names)
   seg_names = []
   for f in img_names:
     # get the filename without the extension
-    basename = os.path.basename(f).split('.')[0]
+    basename = os.path.splitext(os.path.basename(f))[0]
     # cover its corresponding *_seg.png
     seg = os.path.join(dataset_label_dir, basename+'.png')
     seg_names.append(seg)
@@ -87,7 +87,7 @@ def _convert_dataset(dataset_split, dataset_dir, dataset_label_dir):
     output_filename = os.path.join(
         FLAGS.output_dir,
         '%s-%05d-of-%05d.tfrecord' % (dataset_split, shard_id, _NUM_SHARDS))
-    with tf.python_io.TFRecordWriter(output_filename) as tfrecord_writer:
+    with tf.io.TFRecordWriter(output_filename) as tfrecord_writer:
       start_idx = shard_id * num_per_shard
       end_idx = min((shard_id + 1) * num_per_shard, num_images)
       for i in range(start_idx, end_idx):
@@ -95,12 +95,25 @@ def _convert_dataset(dataset_split, dataset_dir, dataset_label_dir):
             i + 1, num_images, shard_id))
         sys.stdout.flush()
         # Read the image.
-        image_filename = img_names[i]
-        image_data = tf.gfile.FastGFile(image_filename, 'rb').read()
+        image_filename,image_data=None,None
+        img_names[i]=os.path.splitext(img_names[i])[0]+".jpg"
+        print(img_names[i])
+        try:
+          image_filename = img_names[i]
+          image_data = tf.compat.v1.gfile.FastGFile(image_filename, 'rb').read()
+        except:
+          img_names[i]=os.path.splitext(img_names[i])[0]+".png"
+          try:
+            image_filename = img_names[i]
+            image_data = tf.compat.v1.gfile.FastGFile(image_filename, 'rb').read()
+          except:
+            img_names[i]=os.path.splitext(img_names[i])[0]+".jpeg"
+            image_filename = img_names[i]
+            image_data = tf.compat.v1.gfile.FastGFile(image_filename, 'rb').read()
         height, width = image_reader.read_image_dims(image_data)
         # Read the semantic segmentation annotation.
         seg_filename = seg_names[i]
-        seg_data = tf.gfile.FastGFile(seg_filename, 'rb').read()
+        seg_data = tf.compat.v1.gfile.FastGFile(seg_filename, 'rb').read()
         seg_height, seg_width = label_reader.read_image_dims(seg_data)
         if height != seg_height or width != seg_width:
           raise RuntimeError('Shape mismatched between image and label.')
@@ -113,11 +126,11 @@ def _convert_dataset(dataset_split, dataset_dir, dataset_label_dir):
 
 
 def main(unused_argv):
-  tf.gfile.MakeDirs(FLAGS.output_dir)
+  tf.io.gfile.makedirs(FLAGS.output_dir)
   _convert_dataset(
       'train', FLAGS.train_image_folder, FLAGS.train_image_label_folder)
   _convert_dataset('val', FLAGS.val_image_folder, FLAGS.val_image_label_folder)
 
 
 if __name__ == '__main__':
-  tf.app.run()
+  tf.compat.v1.app.run()
