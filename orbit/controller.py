8d427547086c11bde31ac69a2cd64996221a940f
TXH2020 <87963860+TXH2020@users.noreply.github.com>
2023-07-03 16:05:03 +0530

    Update controller.py

--- orbit/controller.py
+++ orbit/controller.py
@@ -563,7 +563,7 @@ class Controller:
     # pylint: disable=protected-access
     if self.checkpoint_manager:
       logging.info("Sync on async checkpoint saving.")
-      self.checkpoint_manager.sync()
+      self.checkpoint_manager.save()
 
 
 class StepTimer:
