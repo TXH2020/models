af1572187b94ae032a598ce00d47570e198dfa45
TXH2020 <87963860+TXH2020@users.noreply.github.com>
2023-07-10 18:24:35 +0530

    Update coco2voc.py

--- research/deeplab/datasets/coco2voc/coco2voc.py
+++ research/deeplab/datasets/coco2voc/coco2voc.py
@@ -8,66 +8,106 @@ import time
 import labelme
 
 
-def coco2voc(anns_file, root_folder, mode='train', name_bits=7, n=None, compress=True):
-	'''
-	This function converts COCO style annotations to PASCAL VOC style instance and class
-		segmentations. Additionaly, it creates a segmentation mask(1d ndarray) with every pixel contatining the id of
-		the instance that the pixel belongs to.
-	:param anns_file: COCO annotations file, as given in the COCO data set
-	:param root_folder: path to the folder where the results will be saved
-	:param mode: `train`, 'trainval' or `val`
-	:param n: Number of image annotations to convert. Default is None in which case all of the annotations are converted
-	:param compress: if True, id segmentation masks are saved as '.npz' compressed files. if False they are saved as '.npy'
-	:return: All segmentations are saved to the target folder, along with a list of ids of the images that were converted
-	'''
+def coco2voc1(anns_file, root_folder, mode='train', name_bits=7, n=None, compress=True):
+    coco_instance = COCO(anns_file)
+    coco_imgs = coco_instance.imgs
+    if n is None:
+        n = len(coco_imgs)
+    else:
+        assert type(n) == int, "n must be an int"
+        n = min(n, len(coco_imgs))
 
-	coco_instance = COCO(anns_file)
-	coco_imgs = coco_instance.imgs
+    instance_target_path = os.path.join(root_folder, 'SegmentationObject')
+    class_target_path = os.path.join(root_folder, 'SegmentationClass')
+    id_target_path = os.path.join(root_folder, 'SegmentationId')
+    image_id_list_path = os.path.join(root_folder, 'ImageSets', 'Segmentation', f'{mode}.txt')
 
-	if n is None:
-		n = len(coco_imgs)
-	else:
-		assert type(n) == int, "n must be an int"
-		n = min(n, len(coco_imgs))
+    if not os.path.exists(instance_target_path): os.system(f'mkdir {instance_target_path}')
+    if not os.path.exists(class_target_path): os.system(f'mkdir {class_target_path}')
+    if not os.path.exists(id_target_path): os.system(f'mkdir {id_target_path}')
+    image_id_list = open(image_id_list_path, 'a+')
 
-	instance_target_path = os.path.join(root_folder, 'SegmentationObject')
-	class_target_path = os.path.join(root_folder, 'SegmentationClass')
-	id_target_path = os.path.join(root_folder, 'SegmentationId')
-	image_id_list_path = os.path.join(root_folder, 'ImageSets', 'Segmentation', f'{mode}.txt')
+    start = time.time()
+    for i, img in enumerate(coco_imgs):
+        anns_ids = coco_instance.getAnnIds(img)
+        anns = coco_instance.loadAnns(anns_ids)
+        if not anns:
+            continue
 
-	if not os.path.exists(instance_target_path): os.system(f'mkdir {instance_target_path}')
-	if not os.path.exists(class_target_path): os.system(f'mkdir {class_target_path}')
-	if not os.path.exists(id_target_path): os.system(f'mkdir {id_target_path}')
-	image_id_list = open(image_id_list_path, 'a+')
+        img = str(img).zfill(name_bits)
+        class_seg, instance_seg, id_seg = annsToSeg(anns, coco_instance)
 
-	start = time.time()
-	for i, img in enumerate(coco_imgs):
+        # Image.fromarray(class_seg).convert("L").save(class_target_path + '/' + str(img) + '.png')
+        # Image.fromarray(instance_seg).convert("L").save(instance_target_path + '/' + str(img) + '.png')
+        labelme.utils.lblsave(class_target_path + '/' + str(img) + '.png', class_seg)
+        labelme.utils.lblsave(instance_target_path + '/' + str(img) + '.png', instance_seg)
 
-		anns_ids = coco_instance.getAnnIds(img)
-		anns = coco_instance.loadAnns(anns_ids)
-		if not anns:
-			continue
+        if compress:
+            np.savez_compressed(os.path.join(id_target_path, str(img)), id_seg)
+        else:
+            np.save(os.path.join(id_target_path, str(img) + '.npy'), id_seg)
 
-		img = str(img).zfill(name_bits)
-		class_seg, instance_seg, id_seg = annsToSeg(anns, coco_instance)
+        image_id_list.write(str(img) + '\n')
 
-		# Image.fromarray(class_seg).convert("L").save(class_target_path + '/' + str(img) + '.png')
-		# Image.fromarray(instance_seg).convert("L").save(instance_target_path + '/' + str(img) + '.png')
-		labelme.utils.lblsave(class_target_path + '/' + str(img) + '.png', class_seg)
-		labelme.utils.lblsave(instance_target_path + '/' + str(img) + '.png', instance_seg)
-		
-		if compress:
-			np.savez_compressed(os.path.join(id_target_path, str(img)), id_seg)
-		else:
-			np.save(os.path.join(id_target_path, str(img) + '.npy'), id_seg)
+        if i % 100 == 0 and i > 0:
+            print(str(i) + " annotations processed" +
+                  " in " + str(int(time.time() - start)) + " seconds")
+        if i >= n:
+            break
 
-		image_id_list.write(str(img) + '\n')
+    image_id_list.close()
+    return
 
-		if i % 100 == 0 and i > 0:
-			print(str(i) + " annotations processed" +
-				  " in " + str(int(time.time()-start)) + " seconds")
-		if i >= n:
-			break
+def coco2voc2(anns_file, root_folder, mode='train', name_bits=7, n=None, compress=True):
+    coco_instance = COCO(anns_file)
+    coco_imgs = coco_instance.imgs
+    if n is None:
+        n = len(coco_imgs)
+    else:
+        assert type(n) == int, "n must be an int"
+        n = min(n, len(coco_imgs))
 
-	image_id_list.close()
-	return
+    instance_target_path = os.path.join(root_folder, 'SegmentationObject1')
+    class_target_path = os.path.join(root_folder, 'SegmentationClass1')
+    id_target_path = os.path.join(root_folder, 'SegmentationId1')
+    image_id_path = os.path.join(root_folder, 'ImageSets1')
+    image_id_seg_path=os.path.join(image_id_path, 'Segmentation')
+    image_id_list_path = os.path.join(image_id_seg_path,f'{mode}.txt')
+
+    if not os.path.exists(instance_target_path): os.system(f'mkdir {instance_target_path}')
+    if not os.path.exists(class_target_path): os.system(f'mkdir {class_target_path}')
+    if not os.path.exists(id_target_path): os.system(f'mkdir {id_target_path}')
+    if not os.path.exists(image_id_path): os.system(f'mkdir {image_id_path}')
+    if not os.path.exists(image_id_seg_path): os.system(f'mkdir {image_id_seg_path}')
+    image_id_list = open(image_id_list_path, 'a+')
+
+    start = time.time()
+    for i, img in enumerate(coco_imgs):
+        anns_ids = coco_instance.getAnnIds(img)
+        anns = coco_instance.loadAnns(anns_ids)
+        if not anns:
+            continue
+
+        img = str(img).zfill(name_bits)
+        class_seg, instance_seg, id_seg = annsToSeg(anns, coco_instance)
+
+        # Image.fromarray(class_seg).convert("L").save(class_target_path + '/' + str(img) + '.png')
+        # Image.fromarray(instance_seg).convert("L").save(instance_target_path + '/' + str(img) + '.png')
+        labelme.utils.lblsave(class_target_path + '/' + str(img) + '.png', class_seg)
+        labelme.utils.lblsave(instance_target_path + '/' + str(img) + '.png', instance_seg)
+
+        if compress:
+            np.savez_compressed(os.path.join(id_target_path, str(img)), id_seg)
+        else:
+            np.save(os.path.join(id_target_path, str(img) + '.npy'), id_seg)
+
+        image_id_list.write(str(img) + '\n')
+
+        if i % 100 == 0 and i > 0:
+            print(str(i) + " annotations processed" +
+                  " in " + str(int(time.time() - start)) + " seconds")
+        if i >= n:
+            break
+
+    image_id_list.close()
+    return
